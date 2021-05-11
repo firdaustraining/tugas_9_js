f3042dd195b7fe3440ba266dbde9f754f96c0827
Faza <firdausauliafaza@gmail.com>
2021-05-11 09:04:30 +0700

    niomic.com

---
+++ tugas9.js
@@ -0,0 +1,12 @@
+function firdaus() {
+  var data = {
+    nama: "faza",
+    umur: 21,
+    jurusan: "teknik informatika",
+    semester: 6,
+  };
+  for (var x in data) {
+    console.log(data[x]);
+  }
+}
+firdaus();
